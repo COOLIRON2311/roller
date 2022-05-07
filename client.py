e1d7d854630b4188fed9271f05b79d8aae14df74
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2022-05-07 16:17:50 +0300

    Full api implementation

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import http.client
-import urllib.parse
+import json
 SERVER = 'localhost:8080'
+HEAD = {'Content-type': 'application/json'}
 
 
 def test_roll(dice: int = 20):
@@ -12,14 +13,14 @@ def test_roll(dice: int = 20):
 
 def test_rolls(dice: dict[str, int]):
     con = http.client.HTTPConnection(SERVER)
-    con.request('POST', '/rolls', urllib.parse.urlencode(dice))
+    con.request('POST', '/rolls', json.dumps(dice), headers=HEAD)
     print(con.getresponse().read().decode('utf8'))
     con.close()
 
 
 def main():
     test_roll()
-    # test_rolls({'d6': 1, 'd8': 3})
+    test_rolls({'d6': 1, 'd8': 3})
 
 
 if __name__ == '__main__':
